5280e06e25ffa3feea5cb4f898e435a4330ad6a8
Mustafa Khazaee <101381823+MustafaKhazaee@users.noreply.github.com>
2022-12-24 12:51:53 +0430

    commit build time client generation

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,8 +1,10 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Sarafi.Application.Applications.Accounts.Commands;
 using Sarafi.Application.Applications.Accounts.Dtos;
 using Sarafi.Application.Applications.Accounts.Queries;
+using Sarafi.Domain.Constants;
 
 namespace Sarafi.API.Controllers
 {
@@ -10,7 +12,7 @@ namespace Sarafi.API.Controllers
     {
         public AccountController(IMediator mediator) : base(mediator) { }
 
-        //[HttpPost(nameof(AddAccountAsync)), Authorize(Roles = $"root, {nameof(AddAccountAsync)}")]
+        //[HttpPost(nameof(AddAccountAsync)), Authorize(Roles = AuthorizationRoles.Accounts.Post)]
         [HttpPost(nameof(AddAccountAsync))]
         public async Task<string> AddAccountAsync(AddAccountCommand addAccountCommand) => 
             await mediator.Send(addAccountCommand);
